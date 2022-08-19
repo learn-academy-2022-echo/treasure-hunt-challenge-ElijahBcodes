a98fb26fcc9fcc6e4a81d6e06487f075a7be782f
ElijahBcodes <eli1inthesnow@gmail.com>
2022-08-19 10:51:05 -0700

    user-story-1-2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import Square from './components/Square'
 import './App.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
-    }
-  }
-
-  render() {
+const App = () => {
+  
+const [board, setBoard]= useState(["?", "?", "?", "?", "?", "?", "?", "?", "?"])    
+  
+const handleGamePlay = (index) => {
+        alert(index)    
+ } 
+  
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div className= "boardgame">
+            {board.map((value, index) => {
+              return(
+                <Square value ={value} index={index} handleGamePlay={handleGamePlay}/>
+              )
+          })}
+          </div>
       </>
     )
   }
-}
+
 export default App
